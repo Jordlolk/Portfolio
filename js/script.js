bb0ab6eb9ef1b342a91f8e6207ff45e797ba2ee5
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2024-07-19 20:17:22 -0300

    general updates

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log(window.innerWidth);
+
+let KyloImg = document.getElementById('KyloImg')
+function updateImg(mediaQuery){
+  let a = window.innerWidth
+  if(a <= 623){
+    KyloImg.setAttribute('src' , 'imgs/darthVaderChibi.png')
+  } else{
+    KyloImg.setAttribute('src' , 'imgs/KyloRen.png')
+  }
+}
+const mediaQuery = window.matchMedia('(max-width: 623px)');
+mediaQuery.addEventListener('change' , updateImg)
+updateImg(mediaQuery)
