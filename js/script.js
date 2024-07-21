17f1320c10191692084b92b2e9c306d17da6b17c
jordlolk <jordeallefsilvadecarvalho@gmail.com>
2024-07-20 21:05:01 -0300

    general improving

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 
 let KyloImg = document.getElementById('KyloImg')
-function updateImg(mediaQuery){
-  let a = window.innerWidth
-  if(a <= 623){
+function updateImg(){
+  let windoWidth = window.innerWidth
+  
+  if(windoWidth <= 623){ 
     KyloImg.setAttribute('src' , 'imgs/kyloRenChibi.png')
+
   } else{
     KyloImg.setAttribute('src' , 'imgs/kyloRen.png')
+
   }
 }
 const mediaQuery = window.matchMedia('(max-width: 623px)');
 mediaQuery.addEventListener('change' , updateImg)
-updateImg(mediaQuery)
+updateImg()
